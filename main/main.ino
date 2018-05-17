eb16bc161084c11184a083fd6c65cc6915d1f351
Aero15 <aero15@outlook.com>
2018-05-17 08:48:59 +0400

    Quelques commentaires

--- main/main.ino
+++ main/main.ino
@@ -4,23 +4,23 @@ bouton melodiePredefinie[4]; // contient les boutonsMelodie prédefinis
 bouton melodieSaisie[10]; //contient la saisie du joueur 
 
 void setup() {
-  initMelodie();
-  Serial.begin(9600);
+    initMelodie();
+    Serial.begin(9600);
 }
 
 void loop()
 {
-  activate(melodiePredefinie[0]);
-
+    activate(melodiePredefinie[0]);
 }
 
+void initMelodie() // Charge les mélodies prédéfinies
+{
+    melodiePredefinie[0]=setBouton(2,75,12);
+    melodiePredefinie[1]=setBouton(2,75,12);
+    melodiePredefinie[2]=setBouton(2,75,12);
+    melodiePredefinie[3]=setBouton(2,75,12);
+}
 
-void initMelodie(){
-  melodiePredefinie[0]=setBouton(2,75,12);
-  melodiePredefinie[1]=setBouton(2,75,12);
-  melodiePredefinie[2]=setBouton(2,75,12);
-  melodiePredefinie[3]=setBouton(2,75,12);
-  }
 
 
 
