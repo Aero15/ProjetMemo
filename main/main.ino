6aaacbaed11718bacb27fba36cf49131a6835376
Ismael <ismael.saidpro@gmail.fr>
2018-05-17 08:50:36 +0400

    generer une mélodie

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,10 @@
 #include "boutons.h"
+#include <stdlib.h>
+#include <time.h>
 
 bouton melodiePredefinie[4]; // contient les boutonsMelodie prédefinis
 bouton melodieSaisie[10]; //contient la saisie du joueur 
+bouton melodieGenerer[10];//contient la mélodie générer par le system
 
 void setup() {
   initMelodie();
@@ -15,12 +18,20 @@ void loop()
 }
 
 
-void initMelodie(){
+void initMelodie(){ // Permet d'initialiser un tableau de note prédéfinis
   melodiePredefinie[0]=setBouton(2,75,12);
   melodiePredefinie[1]=setBouton(2,75,12);
   melodiePredefinie[2]=setBouton(2,75,12);
   melodiePredefinie[3]=setBouton(2,75,12);
-  }
+}
+
+
+
+void genereMelodie(){
+  int a =rand()%(10-1)+1;
+    
+}
+
 
 
 
