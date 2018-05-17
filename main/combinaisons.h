1c7ba3de201f08c1ede48c30276d2d729adef268
Aero15 <aero15@outlook.com>
2018-05-17 11:57:14 +0400

    Séparation du code gérant les combinaisons

---
+++ main/combinaisons.h
@@ -0,0 +1,46 @@
+bouton melodiePredefinie[4]; // Contient les Boutons/Led/Frequences prédefinis
+bouton melodieGenerer[10];   // Contient les B/L/F à saisir
+bouton melodieSaisie[10];    // Contient la saisie des B/L/F du joueur 
+
+void initTableaux() // Initialise les tableaux avec des boutons "vides"
+{
+    for(int i=0; i<10;i++)
+    {
+        melodieGenerer[i] = setBouton(0,0,0); // On initialise le tableau melodieGenerer avec des melodie vides
+        melodieSaisie[i]  = setBouton(0,0,0); // On initialise le tableau melodieSaisie avec des melodie vides
+    }
+}
+
+void genererMelodie() // Increment la combinaison de mélodies 
+{
+    int a = rand()%4; 
+    for (int i=0; i<10;i++)
+    {
+        if (melodieGenerer[i].pinLed==0)
+        {
+            melodieGenerer[i]=melodiePredefinie[a];
+            return;
+        }
+    }
+}
+
+void ecouteSaisie() // Ecoute la combinaison des boutons saisie par le joueur
+{
+    // Tant qu'on a pas tout saisi, on ne sort pas de là, pigé !
+}
+
+bool verification() // Vérifie si la combinaison saisie est = à la combinaison générée
+{
+    for(int i=0; i<10; i++)
+    {
+        int b1 = melodieGenerer[i].pinButton;
+        int b2 = melodieSaisie[i].pinButton;
+        
+        if(b1 == 0 || b2 == 0) // Si on rencontre un bouton vide
+            break;             // On sort de la boucle
+        else if(b1 != b2)      // Sinon si on rencontre des boutons différents
+            return false;      // On retourne faux
+    }
+
+    return true;
+}
--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,6 @@
-#include "boutons.h"
 #include <time.h>
+#include "boutons.h"
+#include "combinaisons.h"
 
 #define STATE1 0
 #define STATE2 1
@@ -7,10 +8,6 @@
 #define STATE4 3
 
 int state, nextState;
-
-bouton melodiePredefinie[4]; // Contient les Boutons/Led/Frequences prédefinis
-bouton melodieGenerer[10];   // Contient les B/L/F à saisir
-bouton melodieSaisie[10];    // Contient la saisie des B/L/F du joueur 
 
 void setup()
 {
@@ -58,46 +55,5 @@ void initMelodies() // Charge les mélodies prédéfinies
     melodiePredefinie[3]=setBouton(0,75,12);
 }
 
-void initTableaux() // Initialise les tableaux avec des boutons "vides"
-{
-    for(int i=0; i<10;i++)
-    {
-        melodieGenerer[i] = setBouton(0,0,0); // On initialise le tableau melodieGenerer avec des melodie vides
-        melodieSaisie[i]  = setBouton(0,0,0); // On initialise le tableau melodieSaisie avec des melodie vides
-    }
-}
 
-void genererMelodie() // Increment la combinaison de mélodies 
-{
-    int a = rand()%4; 
-    for (int i=0; i<10;i++)
-    {
-        if (melodieGenerer[i].pinLed==0)
-        {
-            melodieGenerer[i]=melodiePredefinie[a];
-            return;
-        }
-    }
-}
-
-void ecouteSaisie() // Ecoute la combinaison des boutons saisie par le joueur
-{
-    // Tant qu'on a pas tout saisi, on ne sort pas de là, pigé !
-}
-
-bool verification() // Vérifie si la combinaison saisie est = à la combinaison générée
-{
-    for(int i=0; i<10; i++)
-    {
-        int b1 = melodieGenerer[i].pinButton;
-        int b2 = melodieSaisie[i].pinButton;
-        
-        if(b1 == 0 || b2 == 0) // Si on rencontre un bouton vide
-            break;             // On sort de la boucle
-        else if(b1 != b2)      // Sinon si on rencontre des boutons différents
-            return false;      // On retourne faux
-    }
-
-    return true;
-}
 
