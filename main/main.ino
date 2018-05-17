0e12171351b9ab1b793515296c97c0e4dac49d5c
Ismael <ismael.saidpro@gmail.fr>
2018-05-17 09:40:14 +0400

    Fonction initTableau
    Fonction qui sert a initialiser les tableau genereTableau et melodie saisie avec des boutons "vide"

--- main/main.ino
+++ main/main.ino
@@ -5,6 +5,7 @@ bouton melodieSaisie[10]; //contient la saisie du joueur
 
 void setup() {
     initMelodie();
+    initTableau();
     Serial.begin(9600);
 }
 
@@ -19,6 +20,14 @@ void initMelodie() // Charge les mélodies prédéfinies
     melodiePredefinie[1]=setBouton(2,75,12);
     melodiePredefinie[2]=setBouton(2,75,12);
     melodiePredefinie[3]=setBouton(2,75,12);
+void initTableau()
+{
+    for(int i=0; i<10;i++)
+    {
+        melodieGenerer[i]=setBouton(0,0,0);//On initialise le tableau melodieGenerer avec des melodie vides
+        melodieSaisie[i]=setBouton(0,0,0);//On initialise le tableau melodieSaisie avec des melodie vides
+    }
+}
 }
 
 
