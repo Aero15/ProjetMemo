1a68a445b9b3ceeec6bb7b92b9a9b73ab35c6bc4
Ismael <ismael.saidpro@gmail.fr>
2018-05-17 09:43:10 +0400

    Fonction genereMelodie
    Fonction qui choisie une melodie dans le tableau de melodiePredefinie
    et l'ajout dans le tableau melodie Generer

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,30 @@
 #include "boutons.h"
+#include <time.h>
 
 bouton melodiePredefinie[4]; // contient les boutonsMelodie prédefinis
 bouton melodieSaisie[10]; //contient la saisie du joueur 
+bouton melodieGenerer[10];//contient la melodie générer 
 
 void setup() {
     initMelodie();
     initTableau();
+    srand(time(NULL));
     Serial.begin(9600);
 }
 
 void loop()
 {
-    activate(melodiePredefinie[0]);
+
 }
 
 void initMelodie() // Charge les mélodies prédéfinies
 {
-    melodiePredefinie[0]=setBouton(2,75,12);
-    melodiePredefinie[1]=setBouton(2,75,12);
-    melodiePredefinie[2]=setBouton(2,75,12);
-    melodiePredefinie[3]=setBouton(2,75,12);
+    melodiePredefinie[0]=setBouton(9,75,12);
+    melodiePredefinie[1]=setBouton(1,75,12);
+    melodiePredefinie[2]=setBouton(4,75,12);
+    melodiePredefinie[3]=setBouton(0,75,12);
+}
+
 void initTableau()
 {
     for(int i=0; i<10;i++)
@@ -28,6 +33,19 @@ void initTableau()
         melodieSaisie[i]=setBouton(0,0,0);//On initialise le tableau melodieSaisie avec des melodie vides
     }
 }
+
+void genereMelodie()
+{
+    
+    int a =rand()%4; 
+    for (int i=0; i<10;i++)
+    {
+        if (melodieGenerer[i].pinLed==0)
+        {
+            melodieGenerer[i]=melodiePredefinie[a];
+            return;
+        }
+    }
 }
 
 
